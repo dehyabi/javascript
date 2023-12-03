132cca692f202fba0449a3be07fe28547eb0c949
dehyabi <dehyasvibe@gmail.com>
2023-12-03 08:09:09 +0000

    show data to DOM

--- index.js
+++ index.js
@@ -7,10 +7,12 @@
 
 let firstName = "Dehya"; //strings
 let age = 31; //numbers
-let teacher = false; //booleans
+let teacher = true; //booleans
 
 console.log("Hello", firstName);
 console.log("You are", age, "years old");
 console.log("enrolled:", teacher);
 
 document.getElementById("p1").innerHTML= "Hello " + firstName;
+document.getElementById("p2").innerHTML= "You are " + age + " years old";
+document.getElementById("p3").innerHTML= "Enrolled: " + teacher;
