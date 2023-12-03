edee167ef696b28fd2938d585402a26923b9b833
dehyabi <dehyasvibe@gmail.com>
2023-12-03 09:33:46 +0000

    arithmetic

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-// A variable is a container for storing data
-// A variable behaves as if it was the value that it contains
+/*
+ arithmetic is a combination of 
+ operands (values, variables, etc.)
+ operators (+ - * / %)
+ that can be evaluated to a value
+ ex. y = x + 5;
+*/
 
-// Two steps:
-// 1. Declarations (var, let, const)
-// 2. Assignment (= assignment operator)
 
-let firstName = "Dehya"; //strings
-let age = 31; //numbers
-let teacher = true; //booleans
 
-console.log("Hello", firstName);
-console.log("You are", age, "years old");
-console.log("enrolled:", teacher);
+let students = 42;
 
-document.getElementById("p1").innerHTML= "Hello " + firstName;
-document.getElementById("p2").innerHTML= "You are " + age + " years old";
-document.getElementById("p3").innerHTML= "Enrolled: " + teacher;
+//students = students + 1;
+//students = students - 1;
+//students = students * 3;
+//students = students / 2;
+
+//let extraStudents = students % 3;
+
+//students += 1;
+//students -= 1;
+//students *= 4;
+//students /= 4;
+
+console.log(students);
