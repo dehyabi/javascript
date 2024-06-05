fe41340830a83d0dc9582e569942f371fe12d60a
Dehya Qalbi <dehyasvibe@gmail.com>
2024-06-06 00:50:41 +0700

    nested object iteration

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let cars = {
+    label: 'Autos',
+    subs: [
+        {
+          label: 'SUVs',
+          subs: []
+        },
+        {
+          label: 'Trucks',
+          subs: [
+            {
+              label: '2 Wheel Drive',
+              subs: []
+            },
+            {
+              label: '4 Wheel Drive',
+              subs: [
+                {
+                  label: 'Ford',
+                  subs: []
+                },
+                {
+                  label: 'Chevrolet',
+                  subs: []
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: 'Sedan',
+          subs: []
+        }
+    ]
+
+}
+
+function iterateObject(obj) {
+    for (let key in obj) {
+        if(typeof obj[key] === "object" && obj[key] !== null) {
+            iterateObject(obj[key]); // recursive call for nested objects
+        } else {
+            console.log(`key: ${obj[key]}`);
+        }
+    }
+}
+
+iterateObject(cars);
